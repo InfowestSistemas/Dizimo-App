7fffa93ff4cd5399a1e6ef4750659d539966624f
Francisleix <francislei.xavier.ti@gmail.com>
2022-07-06 19:39:20 -0300

    Ticket #10714 - Add script de criação de tabelas pessoa e endereço

--- DizimoAppSoluction/DizimoApp.DAL/ScripsSQL/SQLQuery_CriaTable_Endereco.sql
+++ DizimoAppSoluction/DizimoApp.DAL/ScripsSQL/SQLQuery_CriaTable_Endereco.sql
@@ -1,2 +1,28 @@
 -- Criação de tabela Endereco
- 
+ USE [DizimoDB]
+GO
+
+/****** Object:  Table [dbo].[Endereço]    Script Date: 06/07/2022 19:37:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Endereço](
+	[Id] [int] NOT NULL,
+	[IdPessoa] [int] NULL,
+	[Rua] [varchar](300) NULL,
+	[Numero] [varchar](50) NULL,
+	[Bairro] [varchar](300) NULL,
+	[Cep] [varchar](50) NULL,
+	[Cidade] [varchar](300) NULL,
+	[DataCadastro] [datetime2](7) NULL,
+ CONSTRAINT [PK_Endereço] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
--- DizimoAppSoluction/DizimoApp.DAL/ScripsSQL/SQLQuery_CriaTable_Pessoa.sql
+++ DizimoAppSoluction/DizimoApp.DAL/ScripsSQL/SQLQuery_CriaTable_Pessoa.sql
@@ -1,1 +1,25 @@
 -- Criação de tabela Pessoa
+USE [DizimoDB]
+GO
+
+/****** Object:  Table [dbo].[Pessoa]    Script Date: 06/07/2022 19:28:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Pessoa](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](250) NOT NULL,
+	[Telefone] [varchar](10) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[DataCadastro] [datetime] NOT NULL,
+ CONSTRAINT [PK_dbPessoa] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
