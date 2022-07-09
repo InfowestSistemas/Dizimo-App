cca051c0356fba94eaa923814bab2c0258e3c2f1
Francisleix <francislei.xavier.ti@gmail.com>
2022-07-09 15:55:10 -0300

    Ticket #10721 - Foram adicianados os scripts para as tabelas FluxoCaixa, FluxoTipo, PessoaCategoria

---
+++ DizimoAppSoluction/DizimoApp.DAL/ScripsSQL/06_SQLQuery_CriaTable_Fluxocaixa.sql
@@ -0,0 +1,24 @@
+USE [DizimoDB]
+GO
+
+/****** Object:  Table [dbo].[FluxoCaixa]    Script Date: 09/07/2022 10:33:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[FluxoCaixa](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[IdPessoa] [int] NOT NULL,
+	[DizimoCategoria] [varchar](50) NOT NULL,
+	[Valor] [decimal](18, 0) NOT NULL,
+	[DataCadastro] [datetime] NOT NULL,
+ CONSTRAINT [PK_FluxoCaixa] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
