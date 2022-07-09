a85a7d5f212be76cb85ff66db32f6391c01ed6d2
Reginaldo Teixeira <reginaldo.teixeira.ti@live.com>
2022-07-09 20:11:21 +0100

    Fix Erro Commit 01

---
+++ DizimoAppSoluction/DizimoApp.Dominio/FluxoTipo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizimoApp.Dominio
+{
+    public class FluxoTipo
+    {
+        public int ID { get; set; }
+
+        public string Descricao { get; set; }
+
+        public class FluxoTipoCategoriaCollection : List<FluxoTipo>
+        {
+
+        }
+
+    }
+}
---
+++ DizimoAppSoluction/DizimoApp.Dominio/PessoaCategoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizimoApp.Dominio
+{
+    public class PessoaCategoria
+    {
+        public int ID { get; set; }
+
+        public string Descricao { get; set; }
+
+        public class PessoaCategoriaCollection : List<PessoaCategoria>
+        {
+
+        }
+    }
+}
