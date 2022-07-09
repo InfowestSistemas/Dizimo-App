c8393869f8881dcf3ecca38fbc55d9bbfea342b6
Francisleix <francislei.xavier.ti@gmail.com>
2022-07-08 21:31:43 -0300

    Ticket #10720 - Foi criado as classes PessoaCategoria, FluxoTipo, FluxoCaixa na camada de domínio.

---
+++ DizimoAppSoluction/DizimoApp.Dominio/Fluxocaixa.cs
@@ -0,0 +1,27 @@
+﻿using DizimoApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizimoApp.Dominio
+{
+    public class Fluxocaixa
+    {
+        public int ID { get; set; }
+
+        public int IdPessoa { get; set; }
+
+        public string DizimoCategoria { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public class FluxocaixaCollection : List<Fluxocaixa>
+        {
+
+        }
+    }
+} 
